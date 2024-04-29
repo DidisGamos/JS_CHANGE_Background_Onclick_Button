4362035a6b5c19277101439935810dc5af6b2ca6
DidisGamos <didisgamos@gmail.com>
2024-04-29 20:01:58 +0300

    changes

---
+++ script_bg.js
@@ -0,0 +1,9 @@
+const body = document.querySelector('body');
+const bouton = document.getElementById('bouton');
+const colors = ['White', 'Red', 'Blue', 'Violet', 'Green', 'yellow'];
+
+bouton.addEventListener('click', function () {
+    const multicolor = Math.floor(Math.random() * colors.length);
+    document.body.style.backgroundColor = colors[multicolor];
+    document.querySelector('span').innerHTML = colors[multicolor];
+})
